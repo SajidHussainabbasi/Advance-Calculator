554e5c9f1fb0851b00fc1c8bc06530b826c8e6b8
SajidHussainabbasi <sajid_abbasi2000@yahoo.com>
2025-09-22 14:04:24 +0200

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
+/*➕ Advance Calculator Concepts
+used: Methods, parameters, return types.
+Create separate methods for addition,
+subtraction, multiplication, division , and modulus.
+Make it works for integers and doubles separately (method overloading).
+Ask the user for two numbers and an operation.
+ Call the correct method and return the result.
+  Do integer or double operations based on user input.
+  Example: public static int add(int a, int b) { return a + b; }
+  public static double add(double a, double b) { return a + b; }
+  */
 import java.util.Scanner;
 
 public class Main
